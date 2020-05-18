f1eadd18eb4ae74de66582c317d9e1b56b3c2feb
niktar <niktar-sch@bk.ru>
2020-05-18 22:15:47 +0300

    b4-12

---
+++ find_athelete.py
@@ -0,0 +1,67 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+import datetime
+import users
+
+class Athelete(users.Base):
+    """
+    Описывает структуру таблицы athelete для хранения данных по спортсменам
+    """
+    # задаем название таблицы
+    __tablename__ = 'athelete'
+    # идентификатор пользователя, первичный ключ
+    id = sa.Column(sa.Integer, primary_key=True)
+    # возраст
+    age = sa.Column(sa.Integer)
+    # дата рождения
+    birthdate = sa.Column(sa.Text)
+    # пол
+    gender = sa.Column(sa.Text)
+    # рост
+    height = sa.Column(sa.Float)
+    # вес
+    weight = sa.Column(sa.Integer)
+    # имя
+    name = sa.Column(sa.Text)
+    # количество золотых медалей
+    gold_medals = sa.Column(sa.Integer)
+    # количество серебряных медалей
+    silver_medals = sa.Column(sa.Integer)
+    # количество бронзовых медалей
+    bronze_medals = sa.Column(sa.Integer)
+    # общее количество медалей
+    total_medals = sa.Column(sa.Integer)
+    # вид спорта
+    sport = sa.Column(sa.Text)
+    # страна
+    country = sa.Column(sa.Text)
+
+
+def request_data():
+    """
+    Запрашивает у пользователя user_id
+    """
+    print("Поиск спортсменов, похожих на одного из пользователей.")
+    user_id = input("Ввети идентификатор пользователя: ")
+    return int(user_id)
+
+def main():
+    session = users.connect_db()
+    user_id = request_data()
+    user = session.query(users.User).filter(users.User.id == user_id).first()
+    if user:
+        nearest_birth_athelete = session.query(Athelete)\
+                                 .filter(Athelete.birthdate.isnot(None))\
+                                 .order_by(func.abs(func.julianday(Athelete.birthdate) - func.julianday(user.birthdate))).first()
+        nearest_height_athelete = session.query(Athelete)\
+                                  .filter(Athelete.height.isnot(None))\
+                                  .order_by(func.abs(Athelete.height - user.height)).first()
+        print(f'Ближайший по дате рождения атлет: id: {nearest_birth_athelete.id}, имя: {nearest_birth_athelete.name}, дата рождения: {nearest_birth_athelete.birthdate}')
+        print(f'Ближайший по дате рождения атлет: id: {nearest_height_athelete.id}, имя: {nearest_height_athelete.name}, рост: {nearest_height_athelete.height}')
+    else:
+        print('Пользовалель с таким id не найден')
+
+if __name__ == "__main__":
+    main()
---
+++ users.py
@@ -0,0 +1,86 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+
+# константа, указывающая способ соединения с базой данных
+DB_PATH = "sqlite:///sochi_athletes.sqlite3"
+# базовый класс моделей таблиц
+Base = declarative_base()
+
+class User(Base):
+    """
+    Описывает структуру таблицы user для хранения регистрационных данных пользователей
+    """
+    # задаем название таблицы
+    __tablename__ = 'user'
+    # идентификатор пользователя, первичный ключ
+    id = sa.Column(sa.Integer, primary_key=True)
+    # имя пользователя
+    first_name = sa.Column(sa.Text)
+    # фамилия пользователя
+    last_name = sa.Column(sa.Text)
+    # пол пользователя
+    gender = sa.Column(sa.Text)
+    # адрес электронной почты пользователя
+    email = sa.Column(sa.Text)
+    # дата рождения пользователя
+    birthdate = sa.Column(sa.Date)
+    # рост пользователя
+    height = sa.Column(sa.Float)
+
+
+def connect_db():
+    """
+    Устанавливает соединение к базе данных, создает таблицы, если их еще нет и возвращает объект сессии 
+    """
+    # создаем соединение к базе данных
+    engine = sa.create_engine(DB_PATH, echo=False)
+    # создаем описанные таблицы
+    Base.metadata.create_all(engine)
+    # создаем фабрику сессию
+    session = sessionmaker(engine)
+    # возвращаем сессию
+    return session()
+
+
+def request_data():
+    """
+    Запрашивает у пользователя данные и добавляет их в список users
+    """
+    # выводим приветствие
+    print("Привет! Я запишу твои данные!")
+    # запрашиваем у пользователя данные
+    first_name = input("Введи своё имя: ")
+    last_name = input("А теперь фамилию: ")
+    gender = input("Введи свой пол (male/female): ")
+    email = input("Мне еще понадобится адрес твоей электронной почты: ")
+    birthdate = input("А теперь дату рождения (ГГГГ-ММ-ДД): ")
+    height = input("И еще рост в метрах: ")
+    # создаем нового пользователя
+    user = User(
+        first_name=first_name,
+        last_name=last_name,
+        gender=gender,
+        email=email,
+        birthdate=datetime.datetime.strptime(birthdate, '%Y-%m-%d').date(),
+        height=height
+    )
+    # возвращаем созданного пользователя
+    return user	
+
+def main():
+    """
+    Осуществляет взаимодействие с пользователем, обрабатывает пользовательский ввод
+    """
+    session = connect_db()
+    # запрашиваем данные пользоватлея
+    user = request_data()
+    # добавляем нового пользователя в сессию
+    session.add(user)
+    # сохраняем все изменения, накопленные в сессии
+    session.commit()
+    print("Спасибо, данные сохранены!")
+
+if __name__ == "__main__":
+    main()
